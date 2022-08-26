54a40ab6a765a5b2b7c906a3d69559b081e9f4d9
KanimozhiPalaniappan <spkanimozhi004@gmail.com>
2022-08-26 10:54:46 +0530

    new coding

---
+++ src/com/last/Last.java
@@ -0,0 +1,35 @@
+package com.last;
+import java.util.Iterator;
+
+
+public class Last {
+
+
+
+
+		
+		
+
+
+			public static void main(String[] args) {
+				
+		        int a[]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		    int sum=0;
+
+		    for (int i = 0; i<=10; i++) {
+		    	sum += i;
+			}
+			System.out.println(sum);
+
+		    int n=a.length;
+
+			int average= sum/n;	
+
+				System.out.println(average);
+			}
+
+		}
+
+
+
+
